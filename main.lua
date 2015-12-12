d2d2e9406fb04b361d4f49c59cc8e3e546215bec
David Fu <davidf1212@gmail.com>
2015-12-12 15:19:44 -0500

    wheel joints

--- main.lua
+++ main.lua
@@ -31,8 +31,17 @@ function love.load()
     world = love.physics.newWorld(0, 640, true)
     world:setCallbacks(beginContact, endContact, preSolve, postSolve)
 
+    base = {}
+    base.body = love.physics.newBody(world, sw / 2, sh, 'static')
+    base.shape = love.physics.newCircleShape(4)
+    base.fixture = love.physics.newFixture(base.body, base.shape)
+    base.fixture:setUserData({
+        name = 'base',
+        body = base.body
+    })
+
     floor = {}
-    floor.body = love.physics.newBody(world, sw / 2, sh / 2, 'dynamic')
+    floor.body = love.physics.newBody(world, sw / 2, sh - 80, 'dynamic')
     floor.shape = love.physics.newRectangleShape(640, 8)
     floor.fixture = love.physics.newFixture(floor.body, floor.shape)
     floor.fixture:setUserData({
@@ -41,7 +50,7 @@ function love.load()
     })
 
     fulcrum = {}
-    fulcrum.body = love.physics.newBody(world, sw / 2, sh / 2, 'static')
+    fulcrum.body = love.physics.newBody(world, sw / 2, sh - 80, 'dynamic')
     fulcrum.shape = love.physics.newCircleShape(4)
     fulcrum.fixture = love.physics.newFixture(fulcrum.body, fulcrum.shape)
     fulcrum.fixture:setUserData({
@@ -50,9 +59,9 @@ function love.load()
     })
 
     lsupport = {}
-    lsupport.body = love.physics.newBody(world, sw / 2 - 160, sh / 2 + 80, 'dynamic')
-    lsupport.body:setFixedRotation(true)
-    lsupport.shape = love.physics.newRectangleShape(8, 160)
+    lsupport.body = love.physics.newBody(world, sw / 2 - 160, sh - 40, 'dynamic')
+    lsupport.body:setGravityScale(0)
+    lsupport.shape = love.physics.newRectangleShape(8, 80)
     lsupport.fixture = love.physics.newFixture(lsupport.body, lsupport.shape)
     lsupport.fixture:setUserData({
         name = 'lsupport',
@@ -60,37 +69,50 @@ function love.load()
     })
 
     rsupport = {}
-    rsupport.body = love.physics.newBody(world, sw / 2 + 160, sh / 2 + 80, 'dynamic')
-    rsupport.body:setFixedRotation(true)
-    rsupport.shape = love.physics.newRectangleShape(8, 160)
+    rsupport.body = love.physics.newBody(world, sw / 2 + 160, sh - 40, 'dynamic')
+    rsupport.body:setGravityScale(0)
+    rsupport.shape = love.physics.newRectangleShape(8, 80)
     rsupport.fixture = love.physics.newFixture(rsupport.body, rsupport.shape)
     rsupport.fixture:setUserData({
         name = 'rsupport',
         body = rsupport.body
     })
 
-    love.physics.newRevoluteJoint(fulcrum.body, floor.body, fulcrum.body:getX(), fulcrum.body:getY(), false)
-    love.physics.newRevoluteJoint(lsupport.body, floor.body, lsupport.body:getX(), lsupport.body:getY() - 80, false)
-    love.physics.newRevoluteJoint(rsupport.body, floor.body, rsupport.body:getX(), rsupport.body:getY() - 80, false)
-
     ball = {}
     ball.body = love.physics.newBody(world, sw / 2 - 100, sh / 2 - 100, 'dynamic')
     ball.body:setLinearDamping(0.1)
     ball.shape = love.physics.newCircleShape(16)
     ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+    ball.fixture:setRestitution(0.2)
     ball.fixture:setUserData({
         name = 'ball',
         body = ball.body
     })
+
+    love.physics.newWheelJoint(floor.body, lsupport.body, lsupport.body:getX(), floor.body:getY(), -1, 0)
+    love.physics.newWheelJoint(floor.body, rsupport.body, rsupport.body:getX(), floor.body:getY(), 1, 0)
+    love.physics.newRevoluteJoint(floor.body, fulcrum.body, floor.body:getX(), floor.body:getY())
+    love.physics.newPrismaticJoint(fulcrum.body, base.body, sw / 2, sh, 0, 1)
+    lprism = love.physics.newPrismaticJoint(base.body, lsupport.body, lsupport.body:getX(), sh, 0, -1)
+    lprism:setLimitsEnabled(true)
+    lprism:setLimits(0, 160)
+    rprism = love.physics.newPrismaticJoint(base.body, rsupport.body, rsupport.body:getX(), sh, 0, -1)
+    rprism:setLimitsEnabled(true)
+    rprism:setLimits(0, 160)
 end
 
 function love.update(dt)
     world:update(dt)
+    local force = 1000
     if love.keyboard.isDown('z') then
-        lsupport.body:applyForce(0, 1000)
+        lsupport.body:applyForce(0, force)
+    else
+        lsupport.body:applyForce(0, -force)
     end
     if love.keyboard.isDown('m') then
-        rsupport.body:applyForce(0, 1000)
+        rsupport.body:applyForce(0, force)
+    else
+        rsupport.body:applyForce(0, -force)
     end
 end
 
